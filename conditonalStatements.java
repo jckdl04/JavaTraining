e8db07179f092ef72a10403657cf8f6769e0129b
ffdelemos <ffdelemos@FFDELEMOS1-LTP.headoffice.corp.bpi.com.ph>
2025-12-04 10:41:31 +0800

    Testing

--- conditonalStatements.java
+++ conditonalStatements.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class conditonalStatements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub1111
 		Scanner input = new Scanner(System.in);
 		
 		
